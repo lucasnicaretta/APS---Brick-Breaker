07d8dd014d84c1cdca003890cbb29f0bf359618e
gutiisccp <gabriel.gbogones@outlook.com>
2023-05-21 19:22:18 -0300

    APS - Recycle Bricker

---
+++ unity-brick-breaker-tutorial-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball { get; private set; }
+    public Paddle paddle { get; private set; }
+    public Brick[] bricks { get; private set; }
+
+    const int NUM_LEVELS = 7;
+
+    public int level = 1;
+    public int score = 0;
+    public int lives = 3;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        score = 0;
+        lives = 3;
+
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+
+        if (level > NUM_LEVELS)
+        {
+            // Recomece no nível 1 depois de ter vencido todos os níveis
+            LoadLevel(1);
+            return;
+        }
+
+        SceneManager.LoadScene("Level" + level);
+    }
+
+    private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ball = FindObjectOfType<Ball>();
+        paddle = FindObjectOfType<Paddle>();
+        bricks = FindObjectsOfType<Brick>();
+    }
+
+    public void Miss()
+    {
+        lives--;
+
+        if (lives > 0) {
+            ResetLevel();
+        } else {
+            GameOver();
+        }
+    }
+
+    private void ResetLevel()
+    {
+        paddle.ResetPaddle();
+        ball.ResetBall();
+    }
+
+    private void GameOver()
+    {
+        // Começar um novo jogo
+        NewGame();
+    }
+
+    public void Hit(Brick brick)
+    {
+        score += brick.points;
+
+        if (Cleared()) {
+            LoadLevel(level + 1);
+        }
+    }
+
+    private bool Cleared()
+    {
+        for (int i = 0; i < bricks.Length; i++)
+        {
+            if (bricks[i].gameObject.activeInHierarchy && !bricks[i].unbreakable) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
---
+++ unity-brick-breaker-tutorial-main/Assets/Scripts/TouchMove.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMove : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public Vector2 direction { get; private set; }
+    public float speed = 30f;
+    public float maxBounceAngle = 75f;
+    private int auxDirecao;
+    
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        ResetPaddle();
+    }
+
+     public void ResetPaddle()
+    {
+        rigidbody.velocity = Vector2.zero;
+        transform.position = new Vector2(0f, transform.position.y);
+    }
+
+    void Update()
+    {
+        if (auxDirecao != 0) 
+        {
+            transform.Translate(speed * Time.deltaTime + auxDirecao, 0, 0);
+        }
+
+        if (auxDirecao < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+     
+        if (auxDirecao > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            direction = Vector2.left;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            direction = Vector2.right;
+        } else {
+            direction = Vector2.zero;
+        }
+    }
+
+     private void FixedUpdate()
+    {
+        if (direction != Vector2.zero) {
+            rigidbody.AddForce(direction * speed);
+        }
+
+
+    }
+
+    public void TouchHorizontal(int direcao)
+    {
+        auxDirecao = direcao;
+        speed = 30f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null)
+        {
+            Vector2 paddlePosition = transform.position;
+            Vector2 contactPoint = collision.GetContact(0).point;
+
+            float offset = paddlePosition.x - contactPoint.x;
+            float maxOffset = collision.otherCollider.bounds.size.x / 2;
+
+            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.rigidbody.velocity);
+            float bounceAngle = (offset / maxOffset) * maxBounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -maxBounceAngle, maxBounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.rigidbody.velocity = rotation * Vector2.up * ball.rigidbody.velocity.magnitude;
+        }
+    }
+
+}
